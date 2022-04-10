6f9e896d1a242deac26e40f939686b48bbff012c
ynfeng <fynwin@gmail.com>
2022-04-10 17:23:54 +0800

    chore: modify release

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,7 @@ fn main() {
     let args: Cli = Cli::parse();
 
     let api = HttpCapsuleApi {
-        uri: "https://api.capsuleapp.cyou".to_string(),
+        uri: "http://api.capsuleapp.cyou".to_string(),
         timeout: Duration::from_secs(5),
     };
 
